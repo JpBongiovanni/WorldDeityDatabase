2e139412f71d99080aafce884f24db83eab2822d
Jason Bongiovanni <jpbongiovanni@gmail.com>
2021-08-20 10:48:26 -0400

    added logging to settings

--- coding_dojo_final_project/settings.py
+++ coding_dojo_final_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e-h$099gfvy6d!^q^s189bv361ni1w+$=%r0s0k%ri8mgum+gs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
@@ -140,3 +140,43 @@ DATABASES['default'].update(db_from_env)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+LOGGING = {
+'version': 1,
+'disable_existing_loggers': False,
+'formatters': {
+    'verbose': {
+        'format': ('%(asctime)s [%(process)d] [%(levelname)s] '
+                   'pathname=%(pathname)s lineno=%(lineno)s '
+                   'funcname=%(funcName)s %(message)s'),
+        'datefmt': '%Y-%m-%d %H:%M:%S'
+    },
+    'simple': {
+        'format': '%(levelname)s %(message)s'
+    }
+},
+'handlers': {
+    'null': {
+        'level': 'DEBUG',
+        'class': 'logging.NullHandler',
+    },
+    'console': {
+        'level': 'INFO',
+        'class': 'logging.StreamHandler',
+        'formatter': 'verbose'
+    }
+},
+'loggers': {
+    'django': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+        'propagate': True,
+    },
+    'django.request': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+        'propagate': False,
+    },
+}}
+
+django_heroku.settings(config=locals(), staticfiles=False,logging=False)
